9d0480c6fc9dbd2c13c2026cf477c4f6ec16d7dd
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-04-07 11:38:54 +0300

    Update the model

--- common/models/Company.php
+++ common/models/Company.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace common\models;
+namespace app\models;
+
+use Yii;
 
 /**
  * This is the model class for table "company".
@@ -17,9 +19,12 @@ namespace common\models;
  * @property string $openerp_database_created
  * @property integer $token_key_id
  * @property integer $industry_id
+ * @property integer $token_customer_id
  *
- * @property Industry $industry
  * @property TokenKey $tokenKey
+ * @property Industry $industry
+ * @property TokenCustomer $tokenCustomer
+ * @property CompanyPasswords[] $companyPasswords
  * @property CostbenefitCalculation[] $costbenefitCalculations
  * @property Order[] $orders
  * @property Remark $remark
@@ -27,22 +32,22 @@ namespace common\models;
  */
 class Company extends \yii\db\ActiveRecord
 {
-	/**
-	 * @inheritdoc
-	 */
-	public static function tableName()
-	{
-		return 'company';
-	}
-	
-    public static function getDb()
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
     {
-        return \Yii::$app->db_core;
+        return 'company';
     }
 
-	/**
-	 * @inheritdoc
-	 */
+    public static function getDb()
+    {
+    	return \Yii::$app->db_core;
+    }
+    
+    /**
+     * @inheritdoc
+     */
 	public function rules()
 	{
 		return [
@@ -56,72 +61,89 @@ class Company extends \yii\db\ActiveRecord
 		];
 	}
 
-	/**
-	 * @inheritdoc
-	 */
-	public function attributeLabels()
-	{
-		return [
-			'id' => 'ID',
-			'name' => 'Name',
-			'tag' => 'Tag',
-			'business_id' => 'Business ID',
-			'email' => 'Email',
-			'employees' => 'Employees',
-			'active' => 'Active',
-			'create_time' => 'Create Time',
-			'bank_account_created' => 'Bank Account Created',
-			'openerp_database_created' => 'Openerp Database Created',
-			'token_key_id' => 'Token Key ID',
-			'industry_id' => 'Industry ID',
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'tag' => 'Tag',
+            'business_id' => 'Business ID',
+            'email' => 'Email',
+            'employees' => 'Employees',
+            'active' => 'Active',
+            'create_time' => 'Create Time',
+            'bank_account_created' => 'Bank Account Created',
+            'openerp_database_created' => 'Openerp Database Created',
+            'token_key_id' => 'Token Key ID',
+            'industry_id' => 'Industry ID',
+            'token_customer_id' => 'Token Customer ID',
+        ];
+    }
 
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getIndustry()
-	{
-		return $this->hasOne(Industry::className(), ['id' => 'industry_id']);
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTokenKey()
+    {
+        return $this->hasOne(TokenKey::className(), ['id' => 'token_key_id']);
+    }
 
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getTokenKey()
-	{
-		return $this->hasOne(TokenKey::className(), ['id' => 'token_key_id']);
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getIndustry()
+    {
+        return $this->hasOne(Industry::className(), ['id' => 'industry_id']);
+    }
 
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getCostbenefitCalculations()
-	{
-		return $this->hasMany(CostbenefitCalculation::className(), ['company_id' => 'id']);
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTokenCustomer()
+    {
+        return $this->hasOne(TokenCustomer::className(), ['id' => 'token_customer_id']);
+    }
 
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getOrders()
-	{
-		return $this->hasMany(Order::className(), ['company_id' => 'id']);
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCompanyPasswords()
+    {
+        return $this->hasMany(CompanyPasswords::className(), ['company_id' => 'id']);
+    }
 
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getRemark()
-	{
-		return $this->hasOne(Remark::className(), ['company_id' => 'id']);
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCostbenefitCalculations()
+    {
+        return $this->hasMany(CostbenefitCalculation::className(), ['company_id' => 'id']);
+    }
 
-	/**
-	 * @return \yii\db\ActiveQuery
-	 */
-	public function getSalaries()
-	{
-		return $this->hasMany(Salary::className(), ['company_id' => 'id']);
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrders()
+    {
+        return $this->hasMany(Order::className(), ['company_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRemark()
+    {
+        return $this->hasOne(Remark::className(), ['company_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSalaries()
+    {
+        return $this->hasMany(Salary::className(), ['company_id' => 'id']);
+    }
 }
