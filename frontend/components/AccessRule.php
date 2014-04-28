827186d9e1e37241c480045d5cef8c8cf2b109b5
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-04-28 15:03:43 +0300

    Add an access rule

---
+++ frontend/components/AccessRule.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace frontend\components;
+
+class AccessRule extends \yii\web\AccessRule
+{
+    private $rights = ['user'=>10, 'instructor'=>20, 'manager'=>30, 'admin'=>40];
+    
+    protected function matchRole($user)
+    {
+        if (empty($this->roles)) {
+            return true;
+        }
+        foreach ($this->roles as $role) {
+            if ($role === '?' && $user->getIsGuest()) {
+                return true;
+            } elseif ($role === '@' && !$user->getIsGuest()) {
+                return true;
+            } elseif (!$user->getIsGuest()) {
+                // user is not guest, let's check his role (or do something else)
+                if ($this->rights[$role] === $user->identity->role) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+?>
