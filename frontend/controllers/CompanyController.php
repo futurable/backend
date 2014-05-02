ad67f744a774b5172270b0cf0fb6e93497e777da
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-05-02 14:35:20 +0300

    Add init Add info action Add cbc action

--- frontend/controllers/CompanyController.php
+++ frontend/controllers/CompanyController.php
@@ -2,16 +2,44 @@
 namespace frontend\controllers;
 
 use Yii;
-use yii\base\Controller;
 use common\models\Company;
+use yii\data\ActiveDataProvider;
 
-class CompanyController extends Controller
-{	
+class CompanyController extends MainController
+{
+    private $company;
+    
+    public function actions()
+    {
+        return [
+            'info' => [
+                'class' => 'frontend\controllers\company\InfoAction',
+            ],
+            'costBenefitCalculation' => [
+                'class' => 'frontend\controllers\company\CostBenefitCalculationAction',
+            ],
+        ];
+    }
+    
+    public function init()
+    {
+        if(!yii::$app->user->isGuest){
+            $this->company = Company::findOne(yii::$app->user->identity->company_id);
+        }
+    }
+    
 	public function actionIndex()
 	{
-		$company = Company::find();
+		$company = $this->company;
 	
 		return $this->render('view', ['company'=>$company]);
+	}
+	
+	protected function getCompany(){
+	    if($this->company)
+	    {
+	       return $this->company;
+	    }
 	}
 }
 
