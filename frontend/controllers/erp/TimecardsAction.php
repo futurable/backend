b9b7af1a6da3c7a2466b727064827302b0097aec
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-05-09 13:57:30 +0300

    Fix a typo

--- frontend/controllers/erp/TimecardsAction.php
+++ frontend/controllers/erp/TimecardsAction.php
@@ -4,7 +4,7 @@ namespace frontend\controllers\erp;
 use yii\base\Action;
 use yii\data\ActiveDataProvider;
 
-class TimescardsAction extends Action{
+class TimecardsAction extends Action{
     public function run(){
         return $this->controller->render('timecards', [
 
