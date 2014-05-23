4302b15095c63f7d3c2ae5c26ccc814c42489eda
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-05-23 14:53:45 +0300

    Add employees function

--- frontend/controllers/erp/EmployeesAction.php
+++ frontend/controllers/erp/EmployeesAction.php
@@ -3,11 +3,16 @@ namespace frontend\controllers\erp;
 
 use yii\base\Action;
 use yii\data\ActiveDataProvider;
+use common\models\HrEmployee;
 
 class EmployeesAction extends Action{
     public function run(){
+        $provider = New ActiveDataProvider([
+            'query' => HrEmployee::find(),
+        ]);
+        
         return $this->controller->render('employees', [
-
+            'provider' => $provider,
         ]);
     }
 }
--- frontend/views/erp/employees.php
+++ frontend/views/erp/employees.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\grid\GridView;
 
 /**
  * @var yii\web\View $this
@@ -15,9 +16,23 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?php
+    if($provider->getCount() > 0)
+    {
+        echo GridView::widget([
+            'dataProvider' => $provider,
+                'columns' => [
+                'createU.partner.name:text:'.Yii::t('Backend', 'Creator'),
+                'name_related',
+                'notes',
+            ]
+        ]);
+    }
+    else
+    {
         echo( "<p>" );
         echo Yii::t('Backend', 'No employees.');
         echo( "</p>" );
+    }
     ?>
 
 </div>
