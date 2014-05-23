e9e005e89716acfd7f53dc57f769b0d2e4a685e3
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-05-23 12:20:15 +0300

    Add translations

--- messages/fi/Menu.php
+++ messages/fi/Menu.php
@@ -17,6 +17,8 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return array (
+  'Customers' => 'Asiakkaat',
+  'Suppliers' => 'Toimittajat',
   'Admin actions' => 'Admin toiminnot',
   'Automated orders' => 'Automaattitilaukset',
   'Bank accounts' => 'Pankkitilit',
