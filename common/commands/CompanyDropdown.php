8f4451f8838483c6dc232527bf6c37e9078e78c8
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-05-23 15:35:23 +0300

    Add a dropdown command

---
+++ common/commands/CompanyDropdown.php
@@ -0,0 +1,39 @@
+<?php
+namespace common\commands;
+
+use Yii;
+use common\models\Company;
+use yii\helpers\Url;
+
+class CompanyDropdown
+{
+    public function get(){
+        $companyAccess = new CompanyAccess();
+        
+        $companies = Company::find()
+            ->select(['id','name'])
+            ->where( $companyAccess->getQueryConditions() )
+            ->all();
+        
+        $companyDropdown = []; 
+        
+        foreach($companies as $company){
+            
+            $url_parts = explode("?", Url::canonical());
+            $url_params = "";
+            foreach($_GET as $key => $param){
+                if($key=="id") continue;
+                
+                $url_params .= "$key=$param&";
+            }
+            $url = $url_parts[0]."?".$url_params."id={$company->id}";
+            
+            $companyDropdown[] = [
+                'label'=>$company->name,
+                'url'=> $url,
+            ];
+        }
+        
+        return $companyDropdown;
+    }
+}
