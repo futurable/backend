0dc0f236af53a7d8ed9a434debbca7d36b8eb02d
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-06-17 13:20:28 +0300

    Add breadcrumbs placeholder

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -9,19 +9,21 @@ use common\models\User;
  * @var \common\models\LoginForm $model
  */
 $this->title = Yii::t('Backend', 'Login');
-//$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="jumbotron">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="site-login">
+    <div class="jumbotron">
+        <h1><?= Html::encode($this->title) ?></h1>
+        
+        <div class="row">
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                    <?= $form->field($model, 'username') ?>
+                    <?= $form->field($model, 'password')->passwordInput() ?>
     
-    <div class="row">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton(Yii::t('Backend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+                    <div class="form-group">
+                        <?= Html::submitButton(Yii::t('Backend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    </div>
+                <?php ActiveForm::end(); ?>
+        </div>
     </div>
 </div>
