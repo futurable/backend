649caa73083dab069fecda4980ab09474cf9f8f1
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-06-30 11:07:41 +0300

    Add translations

--- messages/fi/CostbenefitItem.php
+++ messages/fi/CostbenefitItem.php
@@ -9,23 +9,22 @@
  * Each array element represents the translation (value) of a message (key).
  * If the value is empty, the message is considered as not translated.
  * Messages that no longer need translation will have their translations
- * enclosed between a pair of '' marks.
+ * enclosed between a pair of '@@' marks.
  *
  * Message string can be used with plural forms format. Check i18n section
  * of the guide for details.
  *
  * NOTE: this file must be saved in UTF-8 encoding.
  */
-return array (
-  '{CBCType}' => '',
-  'communication' => 'yhteydet',
-  'expenses' => 'materiaalikulut',
-  'health' => 'terveys',
-  'loans' => 'lainat',
-  'otherExpenses' => 'muut kulut',
-  'profit' => 'tuotto',
-  'rents' => 'vuokrat',
-  'salaries' => 'palkat',
-  'sideExpenses' => 'sivukulut',
-  'turnover' => 'liikevaihto',
-);
+return [
+    'communication' => 'yhteydet',
+    'expenses' => 'materiaalikulut',
+    'health' => 'terveys',
+    'loans' => 'lainat',
+    'otherExpenses' => 'muut kulut',
+    'profit' => 'tuotto',
+    'rents' => 'vuokrat',
+    'salaries' => 'palkat',
+    'sideExpenses' => 'sivukulut',
+    'turnover' => 'liikevaihto',
+];
