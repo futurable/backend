22dbaacf6127500d4c89642ee62493a37bf4e9fe
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-07-07 13:53:47 +0300

    Add navigation

--- frontend/controllers/erp/TimesheetsAction.php
+++ frontend/controllers/erp/TimesheetsAction.php
@@ -3,11 +3,56 @@ namespace frontend\controllers\erp;
 
 use yii\base\Action;
 use yii\data\ActiveDataProvider;
+use common\models\AccountAnalyticLine;
+use yii\data\ArrayDataProvider;
+use yii\db\Query;
+use yii\db\Expression;
+use yii\data\Pagination;
 
 class TimesheetsAction extends Action{
     public function run(){
-        return $this->controller->render('timesheets', [
+    
+        $weekNumberQuery = 'to_char(date, \'YYYY-WW\') AS week';
+        $weekNumberExpression = new Expression('to_char(date, \'YYYY-WW\')');
+        
+        $weeksQuery = AccountAnalyticLine::find()->select(["distinct $weekNumberQuery"]);
+        
+        # Calculate the number of navigation buttons
+        $firstDate = AccountAnalyticLine::find()->select('date')->orderBy('date ASC')->one()->date;
+        $lastDate = AccountAnalyticLine::find()->select(['date'])->orderBy('date DESC')->one()->date;
+        
+        $weekNumber = date('Y-W', strtotime($firstDate));
+        
+        $diff = time() - strtotime($firstDate);
+        $weeks = intval( ceil( $diff / (60*60*24*7) ) );
 
+        # Pagination
+        $pages = [];
+
+        while($weeks > 0){
+            $weeks--;
+            $pages[] = ['label' => substr($weekNumber, 5), 'url' => ['timesheets', 'week' => $weekNumber]];
+            
+            $weekNumber++;
+        }
+        
+        
+        $thisWeek = $_GET['week'] ? $_GET['week'] : date('Y-W');
+        $viewWeek = substr($thisWeek, 5, 2) . " / " . substr($thisWeek, 0, 4);
+        
+        $provider = New ActiveDataProvider([
+            'query' =>
+                AccountAnalyticLine::find()
+                ->select(['user_id', "$weekNumberExpression AS week", 'SUM(unit_amount) AS hours_sum'])
+                ->where(['to_char(date, \'YYYY-WW\')' => $thisWeek])
+                ->groupBy(['week', 'user_id']),
+            'pagination' => false
+        ]);
+            
+        return $this->controller->render('timesheets', [
+            'provider' => $provider,
+            'pages' => $pages,
+            'viewWeek' => $viewWeek,
         ]);
     }
 }
--- frontend/views/erp/timesheets.php
+++ frontend/views/erp/timesheets.php
@@ -1,23 +1,51 @@
 <?php
 
 use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\bootstrap\ButtonGroup;
+use yii\widgets\Menu;
+use yii\bootstrap\NavBar;
+use yii\bootstrap\Nav;
 
 /**
  * @var yii\web\View $this
  * @var common\models\Company $model
  */
 
-$this->title = Yii::t('Backend', 'Timesheets') . ", " . yii::$app->session['selected_company_name'];;
+$this->title = Yii::t('Backend', 'Timesheets') . ", " . yii::$app->session['selected_company_name'];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="erp-timesheets">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <h2><?= Yii::t('Backend', 'Week') . "  " . $viewWeek; ?></h2>
+    
     <?php
+    if($provider->getCount() > 0)
+    {
+        echo GridView::widget([
+            'dataProvider' => $provider,
+            'columns' => [
+                'week',
+                'user.partner.name',
+                'hours_sum',
+            ]
+        ]);
+    }
+    else
+    {
         echo( "<p>" );
         echo Yii::t('Backend', 'No timesheets.');
         echo( "</p>" );
+    }
+    
+    NavBar::begin(['brandLabel' => yii::t('Backend', 'Week')]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-left'],
+        'items' => $pages,
+    ]);
+    NavBar::end();
     ?>
 
 </div>
