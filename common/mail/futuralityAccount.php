fcba1668c7c91439dbe8de91fea979580c65aea8
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-08-26 12:33:22 +0300

    Modify formatting Add backend password

--- common/mail/futuralityAccount.php
+++ common/mail/futuralityAccount.php
@@ -15,32 +15,36 @@
     $messageContent .= "</p>";
     
     $messageContent .= "<h2>".Yii::t('Company', 'Bank account')."</h2>";
+    $messageContent .= "<p>";
     $messageContent .= "<ul>";
     $messageContent .= "<li>".Yii::t('Company', 'User account').": <strong>$company->tag</strong></li>";
     $messageContent .= "<li>".Yii::t('Company', 'Password').": <strong>{$company->companyPasswords->bank_password}</strong></li>";
     $messageContent .= "<li>".Yii::t('Company', 'Login from').": <a href='http://futurality.fi/bank/index.php/user/login/?company={$company->tag}'>futurality.fi/bank</a></li>";
     $messageContent .= "</ul>";
+    $messageContent .= "</p>";
     
     $messageContent .= "<h2>".Yii::t('Company', 'OpenERP account')."</h2>";
+    $messageContent .= "<p>";
     $messageContent .= "<ul>";
     $messageContent .= "<li>".Yii::t('Company', 'User account').": <strong>admin</strong></li>";
     $messageContent .= "<li>".Yii::t('Company', 'Password').": <strong>{$company->companyPasswords->openerp_password}</strong></li>";
     $messageContent .= "<li>".Yii::t('Company', 'Login from').": <a href='http://erp.futurality.fi/?db={$company->tag}'>erp.futurality.fi</a></li>";
     $messageContent .= "</ul>";
+    $messageContent .= "</p>";
     
-    /*
     $messageContent .= "<h2>".Yii::t('Company', 'Backend account')."</h2>";
+    $messageContent .= "<p>";
     $messageContent .= "<ul>";
     $messageContent .= "<li>".Yii::t('Company', 'User account').": <strong>{$company->tag}</strong></li>";
     $messageContent .= "<li>".Yii::t('Company', 'Password').": <strong>{$company->companyPasswords->backend_password}</strong></li>";
     $messageContent .= "<li>".Yii::t('Company', 'Login from').": <a href='https://futurality.fi/backend/index.php/site/login?company={$company->tag}'>futurality.fi/backend</a></li>";
     $messageContent .= "</ul>";
-    */
+    $messageContent .= "</p>";
     
     $messageContent .= "<p><strong>".Yii::t('Company', "Have fun")."!</strong></p>";
     
-    $messageContent .= "<p>---</p>";
-    $messageContent .= "<p><a href='http://futurable.fi'>Futurable Oy</a><br/>".date('Y')."</p>";
+    $messageContent .= "<p>---<br/>";
+    $messageContent .= "<a href='http://futurable.fi'>Futurable Oy</a><br/>".date('Y')."</p>";
     
     echo $messageContent;
 ?>
