5d548a40c07ab52cc1a8a25d85ca4de7160a4d91
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-10-09 13:38:02 +0300

    Add login view

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -7,24 +7,22 @@ use yii\widgets\ActiveForm;
  * @var yii\widgets\ActiveForm $form
  * @var \common\models\LoginForm $model
  */
-$this->title = 'Login';
+$this->title = Yii::t('Backend', 'Login');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+    <div class="jumbotron">
+        <h1><?= Html::encode($this->title) ?></h1>
+        
+        <div class="row">
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                    <?= $form->field($model, 'username') ?>
+                    <?= $form->field($model, 'password')->passwordInput() ?>
+    
+                    <div class="form-group">
+                        <?= Html::submitButton(Yii::t('Backend', 'Login'), ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    </div>
+                <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
