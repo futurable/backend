b82f00bd91c24143a00e17c44d0f530611b9395f
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-11-13 12:44:32 +0200

    Add error handling

--- frontend/views/erp/cbc.php
+++ frontend/views/erp/cbc.php
@@ -16,100 +16,105 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php
+    <?php 
+    if($cbc == true){
     
-    GridView::widget([
-            'dataProvider' => $planned,
-            'columns' => [
-                ['label' => Yii::t('Backend', 'Name'), 'value' => function ($data) { return ucfirst(Yii::t('CostbenefitItem', $data->costbenefitItemType->name)); }],
-                ['label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'weekly').")", 'value' => function ($data) { return ( ceil($data->value / 4) )." €"; }],
-                ['label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")", 'value' => function ($data) { return $data->value." €"; }],
-            ],
-        ]);
-
-    /**
-     *  Weekly
-     */
-    echo GridView::widget([
-            'dataProvider' => $cbc,
-            'columns' => [
-                [
-                    'attribute' => 'week',
-                    'contentOptions' => ['class' => 'strong'],
+        GridView::widget([
+                'dataProvider' => $planned,
+                'columns' => [
+                    ['label' => Yii::t('Backend', 'Name'), 'value' => function ($data) { return ucfirst(Yii::t('CostbenefitItem', $data->costbenefitItemType->name)); }],
+                    ['label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'weekly').")", 'value' => function ($data) { return ( ceil($data->value / 4) )." €"; }],
+                    ['label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")", 'value' => function ($data) { return $data->value." €"; }],
                 ],
-                [
-                    'label' => Yii::t('Backend', 'Name'), 
-                    'value' => function ($data) { return ucfirst(Yii::t('CostbenefitItem', $data->type)); }],
-                [
-                    'label' => Yii::t('Backend', 'Planned'), #." (".Yii::t('Backend', 'weekly').")",
-                    'value' => function ($data) { return ($data->planned / 4); },
-                    'format'=>['decimal', 2],
-                    'pageSummary' => true,
-                    'contentOptions' => ['class' => GridView::TYPE_WARNING],
-                ],
-                [
-                    'label' => Yii::t('Backend', 'Realized'), #." (".Yii::t('Backend', 'weekly').")",
-                    'attribute' => 'realized',
-                    #'value' => function ($data) { return ($data->realized)." €"; },
-                    'format'=>['decimal', 2],
-                    'pageSummary' => true,
-                    'contentOptions' => ['class' => GridView::TYPE_INFO],
-                ],
-                [
-                    'label' => Yii::t('Backend', 'Difference'),
-                    'value' => function ($data) { return ( ( $data->realized - ($data->planned / 4)) ); },
-                    'format'=>['decimal', 2],
-                    'pageSummary' => true,
-                    'contentOptions' => function($data){ return getColumnClass($data); },
-                ],
-                /*[
-                    'label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")",
-                    'value' => function ($data) { return ($data->planned)." €"; }
-                ],*/
-            ],
-            'showPageSummary' => true,
-        ]);
+            ]);
     
-    /**
-     *  Total
-     */
-    echo "<h2>".Yii::t('Backend', 'All time')."</h2>";
-    
-    echo GridView::widget([
-            'dataProvider' => $cbcTotal,
-            'columns' => [
-                [
-                    'label' => Yii::t('Backend', 'Name'), 
-                    'value' => function ($data) { return ucfirst(Yii::t('CostbenefitItem', $data->type)); }],
-                [
-                    'attribute' => 'planned',
-                    'label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")",
-                    'format'=>['decimal', 2],
-                    'pageSummary' => true,
-                    'contentOptions' => ['class' => GridView::TYPE_WARNING],
+        /**
+         *  Weekly
+         */
+        echo GridView::widget([
+                'dataProvider' => $cbc,
+                'columns' => [
+                    [
+                        'attribute' => 'week',
+                        'contentOptions' => ['class' => 'strong'],
+                    ],
+                    [
+                        'label' => Yii::t('Backend', 'Name'), 
+                        'value' => function ($data) { return ucfirst(Yii::t('CostbenefitItem', $data->type)); }],
+                    [
+                        'label' => Yii::t('Backend', 'Planned'), #." (".Yii::t('Backend', 'weekly').")",
+                        'value' => function ($data) { return ($data->planned / 4); },
+                        'format'=>['decimal', 2],
+                        'pageSummary' => true,
+                        'contentOptions' => ['class' => GridView::TYPE_WARNING],
+                    ],
+                    [
+                        'label' => Yii::t('Backend', 'Realized'), #." (".Yii::t('Backend', 'weekly').")",
+                        'attribute' => 'realized',
+                        #'value' => function ($data) { return ($data->realized)." €"; },
+                        'format'=>['decimal', 2],
+                        'pageSummary' => true,
+                        'contentOptions' => ['class' => GridView::TYPE_INFO],
+                    ],
+                    [
+                        'label' => Yii::t('Backend', 'Difference'),
+                        'value' => function ($data) { return ( ( $data->realized - ($data->planned / 4)) ); },
+                        'format'=>['decimal', 2],
+                        'pageSummary' => true,
+                        'contentOptions' => function($data){ return getColumnClass($data); },
+                    ],
+                    /*[
+                        'label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")",
+                        'value' => function ($data) { return ($data->planned)." €"; }
+                    ],*/
                 ],
-                [
-                    'label' => Yii::t('Backend', 'Realized')." (".Yii::t('Backend', 'All time').")",
-                    'attribute' => 'realized',
-                    'format'=>['decimal', 2],
-                    'pageSummary' => true,
-                    'contentOptions' => ['class' => GridView::TYPE_INFO],
+                'showPageSummary' => true,
+            ]);
+        
+        /**
+         *  Total
+         */
+        echo "<h2>".Yii::t('Backend', 'All time')."</h2>";
+        
+        echo GridView::widget([
+                'dataProvider' => $cbcTotal,
+                'columns' => [
+                    [
+                        'label' => Yii::t('Backend', 'Name'), 
+                        'value' => function ($data) { return ucfirst(Yii::t('CostbenefitItem', $data->type)); }],
+                    [
+                        'attribute' => 'planned',
+                        'label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")",
+                        'format'=>['decimal', 2],
+                        'pageSummary' => true,
+                        'contentOptions' => ['class' => GridView::TYPE_WARNING],
+                    ],
+                    [
+                        'label' => Yii::t('Backend', 'Realized')." (".Yii::t('Backend', 'All time').")",
+                        'attribute' => 'realized',
+                        'format'=>['decimal', 2],
+                        'pageSummary' => true,
+                        'contentOptions' => ['class' => GridView::TYPE_INFO],
+                    ],
+                    [
+                        'label' => Yii::t('Backend', 'Difference'),
+                        'value' => function ($data) { return ( ( $data->realized - ($data->planned / 4)) ); },
+                        'format'=>['decimal', 2],
+                        'pageSummary' => true,
+                        'contentOptions' => function($data){ return getColumnClass($data); },
+                    ],
+                    /*[
+                        'label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")",
+                        'value' => function ($data) { return ($data->planned)." €"; }
+                    ],*/
                 ],
-                [
-                    'label' => Yii::t('Backend', 'Difference'),
-                    'value' => function ($data) { return ( ( $data->realized - ($data->planned / 4)) ); },
-                    'format'=>['decimal', 2],
-                    'pageSummary' => true,
-                    'contentOptions' => function($data){ return getColumnClass($data); },
-                ],
-                /*[
-                    'label' => Yii::t('Backend', 'Planned')." (".Yii::t('Backend', 'monthly').")",
-                    'value' => function ($data) { return ($data->planned)." €"; }
-                ],*/
-            ],
-            'showPageSummary' => true,
-        ]);
-    
+                'showPageSummary' => true,
+            ]);
+    }
+    else {
+        echo "<p>".Yii::t('Backend', 'No cost-benefit calculation for this company.')."</p>";
+    }
+        
     function getColumnClass($data){
         $class = null;
         
