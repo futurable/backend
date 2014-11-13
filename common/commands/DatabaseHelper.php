f5796c80416545c41c5962c25d6f4eb138a79fec
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-11-13 12:43:03 +0200

    Add validation

--- common/commands/DatabaseHelper.php
+++ common/commands/DatabaseHelper.php
@@ -3,17 +3,38 @@ namespace common\commands;
 
 use Yii;
 use common\models\ResCompany;
+use yii\db\Query;
 
 class DatabaseHelper
 {
+    
     public function changeOdooDBTo($db_name){
         $database = 'db';
         
         return $this->changeTo($db_name, $database);
     }
     
+    public function changeOdooDBToDefault(){
+        $database = 'db';
+        
+        $db_openerp = Yii::$app->get($database);
+        $dsnArray = explode(';',  $db_openerp->dsn);
+        $DBArray = explode('=', $dsnArray[2]);
+        $db_name = $DBArray[1];
+        
+        return $this->changeTo($db_name, $database);
+    }
+    
     public function changeTo($db_name, $database){
-        //  TODO: check if the db exists
+        $query = (new Query())
+        ->select('datname')
+        ->from('pg_database')
+        ->where(['datname'=>$db_name]);
+        
+        $command = $query->createCommand();
+        $rows = $command->queryAll();
+
+        if(empty($rows)) return false;
         
         $db_openerp = Yii::$app->get($database);
         $dsnArray = explode(';',  $db_openerp->dsn);
