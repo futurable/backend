2bed2e54a34386a2d0dde331e714f8301c4f9ba5
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-11-18 12:20:53 +0200

    Reformat

--- common/commands/ConsoleDebug.php
+++ common/commands/ConsoleDebug.php
@@ -4,23 +4,29 @@ namespace common\commands;
 use Yii;
 use yii\helpers\Console;
 
-class ConsoleDebug{
-    
-    public function init(){
+class ConsoleDebug
+{
+
+    public function init()
+    {
         date_default_timezone_set('Europe/Helsinki');
     }
-    
-	public function message($message = false, $debugLevel = 1){
-		if($debugLevel == 0) exit;
-		
-		$date = Console::ansiFormat( date('Y-m-d H:i:s '), [Console::BOLD] );
-		$debugMessage = Console::ansiFormat( $message, [] );
 
-		$output = null;
-		if($message!==false) $output .= $date . $debugMessage;
-		$output .= "\n";
-		
-		Console::stdout($output);
-	}
-
+    public function message($message = false, $debugLevel = 1)
+    {
+        if ($debugLevel == 0)
+            exit();
+        
+        $date = Console::ansiFormat(date('Y-m-d H:i:s '), [
+            Console::BOLD
+        ]);
+        $debugMessage = Console::ansiFormat($message, []);
+        
+        $output = null;
+        if ($message !== false)
+            $output .= $date . $debugMessage;
+        $output .= "\n";
+        
+        Console::stdout($output);
+    }
 }
