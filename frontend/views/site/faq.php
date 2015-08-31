85908e643f9d4d5dca3cdae6e3edb572479a86c5
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-31 15:56:06 +0300

    Add answers

--- frontend/views/site/faq.php
+++ frontend/views/site/faq.php
@@ -11,8 +11,29 @@ $this->params['breadcrumbs'][] = $this->title;
          <h1><?= \Yii::t('Backend', 'Frequentely Asked Questions') ?></h1>
          
          <div id="div-faq">
-             <p class="question">Why is this?</p>
-             <p class="answer">It is what it is.</p>
+             <p class="question">We paid an invoice wrong / twice / etc.</p>
+             <div class="answer">
+                <p>
+                Contact the supplier and settle the matter with them.
+                </p>
+                
+                 <ul>
+                     <li>If the invoice isn't confirmed yet, you can just cancel it and create a new copy.</li>
+                     <li>If the invoice is confirmed, you will need to reconcile the invoice and create a copy.</li>
+                 </ul>
+             </div>
+         </div>
+         
+         <div id="div-faq">
+             <p class="question">The customer hasn't paid their invoice (correctly)</p>
+             <div class="answer">
+                <p>Contact the customer and settle the matter with them.</p>
+             
+                <ul>
+                    <li>If the invoice isn't confirmed yet, you can just cancel it and create a new copy.</li>
+                    <li>If the invoice is confirmed, you will need to reconcile the invoice and create a copy.</li>
+                </ul>
+             </div>
          </div>
          
     </div>
