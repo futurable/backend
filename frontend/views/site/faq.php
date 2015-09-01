606f445038d56127d3589677f7f04e3c72448a0b
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-01 13:37:28 +0300

    Add SO criteria. Realign answers

--- frontend/views/site/faq.php
+++ frontend/views/site/faq.php
@@ -11,20 +11,65 @@ $this->params['breadcrumbs'][] = $this->title;
          <h1><?= \Yii::t('Backend', 'Frequentely Asked Questions') ?></h1>
          
          <div id="div-faq">
-             <p class="question">What is a SO?</p>
+             <p class="question">What are the criteria used to decide the amount of our sales?</p>
              <div class="answer">
-                <p>
-                A Sale Order
-                </p>
+                <p>Each company will get sales whether they improve their company or not.
+                Companies putting effort to their actions get more. "More" sales mean both
+                more orders, but also orders with higher value.</p>
+                
+                <p class='strong'>These will increase your sales:</p>
+                <table>
+                     <tr>
+                        <th>Marketing</th><td>ads, marketing campaigns etc.</td>
+                    </tr>
+                    <tr>
+                        <th>Product pricing</th><td>smart pricing gets more customers</td>
+                    </tr>
+                    <tr>
+                        <th>Customer support</th><td>be professional and friendly</td>
+                        </tr>
+                    <tr>
+                        <th>Paying your employees</th><td>(more) will result in higher employee satisfaction</td>
+                    </tr>
+                    <tr>
+                        <th>Remarks</th><td>instructor can give notes about your company</td>
+                    </tr>
+                    <tr>
+                        <th>Communications</th><td>your data and phone connections</td>
+                    </tr>
+                    <tr>
+                        <th>Revenues</th><td>your company HQ location</td>
+                    </tr>
+                    <tr>
+                        <th>Health</th><td>a health plan increases your employee effiency</td>
+                    </tr>
+                    <tr>
+                        <th>Expenses</th><td>material etc. expenses effect your sales slightly</td>
+                    </tr>
+                    <tr>
+                        <th>Loans</th><td>lower loan amount effects your sales slightly</td>
+                    </tr>
+                    <tr>
+                        <th>Brand</th><td>a derivative from all the above</td>
+                    </tr>
+                </table>
              </div>
          </div>
          
          <div id="div-faq">
-             <p class="question">What is a PO?</p>
+             <p class="question">What does this term mean?</p>
              <div class="answer">
-                <p>
-                A Purchase Order
-                </p>
+                 <table>
+                    <tr>
+                        <th>SO</th><td>Sale Order</td>
+                    </tr>
+                    <tr>
+                        <th>PO</th><td>Purchase Order</td>
+                    </tr>
+                    <tr>
+                        <th>RFQ</th><td>Request for Quotation</td>
+                    </tr>
+                 </table>
              </div>
          </div>
          
